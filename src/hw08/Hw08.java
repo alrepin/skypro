99f1725cbadb594a6071927b32011642d2512f02
alrepin <alrepin@list.ru>
2022-08-01 00:27:44 +0300

    hw08 - на проверку 01

---
+++ src/hw08/Hw08.java
@@ -0,0 +1,9 @@
+package hw08;
+public class Hw08 {
+    public static void info() {
+        System.out.println("Домашнее задание по уроку \"Методы\"");
+    }
+    public static void task1() {
+        System.out.println("Задача 1");
+    }
+}
